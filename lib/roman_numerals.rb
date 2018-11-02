74407b5491f62d6eab2106116d18729ef98e61cd
Tola's Terminal <sohbaker@gmail.com>
2018-11-02 11:52:32 +0000

    works for 15, using math and new array

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,18 +1,33 @@
 class RomanNumerals
   def convert(arabic)
 
+    split_numbers = []
+
     numerals_list = {
-      0 => '',
-      1 => 'I',
-      2 => 'II',
-      3 => 'III',
+      10 => 'X',
+      9 => 'IX',
+      8 => 'VIII',
+      7 => 'VII',
+      6 => 'VI',
+      5 => 'V',
       4 => 'IV',
+      3 => 'III',
+      2 => 'II',
+      1 => 'I'
+      # 0 => ''
     }
 
     numerals_list.each do |key, value|
-      if arabic == key
-        return value # return means I want you to return this thing. different to puts, which is put string.
-      end
+        while arabic >= key
+              p "This is arabic #{arabic}"
+              split_numbers << value
+              p "This is value #{value}"
+              p "This is split_numbers #{split_numbers}"
+              arabic -= key
+              p "This is arabic #{arabic}"
+        end
     end
+      p "This is split_numbers #{split_numbers}"
+      return split_numbers.join
   end
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -6,23 +6,18 @@ RSpec.describe RomanNumerals do
     expect(roman_numerals.convert(0)).to eq('')
   end
 
-  it 'converts 1 to "I"' do
-    roman_numerals = RomanNumerals.new
-    expect(roman_numerals.convert(1)).to eq('I')
-  end
-
-  it 'converts 2 to "II"' do
-    roman_numerals = RomanNumerals.new
-    expect(roman_numerals.convert(2)).to eq('II')
-  end
-
-  it 'converts 3 to "III"' do
-    roman_numerals = RomanNumerals.new
-    expect(roman_numerals.convert(3)).to eq('III')
-  end
-
   it 'converts 4 to "IV"' do
     roman_numerals = RomanNumerals.new
     expect(roman_numerals.convert(4)).to eq('IV')
+  end
+
+  it 'converts 9 to "IX"' do
+    roman_numerals = RomanNumerals.new
+    expect(roman_numerals.convert(9)).to eq('IX')
+  end
+
+  it 'converts 15 to "XV"' do
+    roman_numerals = RomanNumerals.new
+    expect(roman_numerals.convert(15)).to eq('XV')
   end
 end
